ef5f3e46576fb43f4a50dc64ca3ac852bc30a115
Felipe <felipe1180@hotmail.com>
2022-06-08 00:50:28 -0300

    feat(navigation): :sparkles: navigation component

---
+++ src/components/Navigation/index.tsx
@@ -0,0 +1,11 @@
+import { HStack, Link } from "@chakra-ui/react";
+
+export function Navigation() {
+  return (
+    <HStack>
+      <Link>Social Medias</Link>
+      <Link>Experiências</Link>
+      <Link>Curiosidades</Link>
+    </HStack>
+  );
+}
