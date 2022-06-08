9ec156f254d8e537809ceba90bc6b027ee81572a
Felipe <felipe1180@hotmail.com>
2022-06-08 16:08:21 -0300

    feat(experience): :sparkles: redux exp slice

---
+++ src/redux/slices/exp.ts
@@ -0,0 +1,40 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { RootState } from "@redux/store";
+
+const initialState = {
+  level: 1,
+  levelIncreased: false,
+  exp: 0,
+  ExpMultiplicator: 1,
+};
+
+const ExpMultiplicatorByLevel = new Map();
+ExpMultiplicatorByLevel.set(1, 1);
+ExpMultiplicatorByLevel.set(2, 2);
+
+export const expSlice = createSlice({
+  name: "exp",
+  initialState,
+  reducers: {
+    increaseLevel: (state) => {
+      state.level = state.level + 1;
+    },
+    increaseExp: (state) => {
+      if (state.exp === 100) {
+        state.level = state.level + 1;
+        state.levelIncreased = true;
+        state.exp = 0;
+        state.ExpMultiplicator = ExpMultiplicatorByLevel.get(state.level);
+        return;
+      }
+      state.levelIncreased = false;
+      state.exp = state.exp + 10 * state.ExpMultiplicator;
+    },
+  },
+});
+
+export default expSlice.reducer;
+
+export const selectExp = (state: RootState) => state.exp;
+
+export const { increaseExp, increaseLevel } = expSlice.actions;
