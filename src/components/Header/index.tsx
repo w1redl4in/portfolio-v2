9461a8fdb76a8940d3755354d29191664e9719bb
Felipe <felipe1180@hotmail.com>
2022-06-08 16:09:25 -0300

    feat(header): :sparkles: integrated experience

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,40 +1,70 @@
-import Image from "next/image";
-import { Flex, Text, Progress } from "@chakra-ui/react";
+import { Flex, Text, Progress, Box } from "@chakra-ui/react";
 import { Navigation } from "@components/Navigation";
+import { useExperienceInfo } from "@hooks/use-experience-info";
 
 export function Header() {
+  const { isUserAtMaxLevel, exp, level } = useExperienceInfo();
+
   return (
-    <Flex alignItems="center" justifyContent="space-between" p={6}>
-      <Flex
-        justifyContent="space-around"
-        width="12rem"
-        mr={2}
-        alignItems="center"
-      >
-        <Image
-          style={{ borderRadius: "50%" }}
-          width="50px"
-          height="50px"
-          src="https://github.com/w1redl4in.png"
-          alt="minha imagem"
-        />
-        <Text>Felipe Austríaco</Text>
+    <Flex
+      zIndex={999}
+      w="100%"
+      position="fixed"
+      top={0}
+      alignItems="center"
+      py={5}
+      mb={2}
+      justifyContent="space-between"
+    >
+      <Flex justifyContent="space-around" alignItems="center">
+        <Box borderRadius="50%" width="50px" height="50px">
+          {/* <Image
+            style={{ borderRadius: "50%" }}
+            width="50px"
+            height="50px"
+            src="https://github.com/w1redl4in.png"
+            alt="minha imagem"
+          /> */}
+        </Box>
+        <Text casing="uppercase" fontSize="3xl">
+          Felipe Austríaco
+        </Text>
       </Flex>
 
-      <Flex textAlign="center" minW="20rem" flexDirection="column">
-        <Text color="yellow.500" fontWeight="extrabold" colorScheme="yellow">
-          NIVEL 1
+      <Flex
+        alignContent="center"
+        textAlign="center"
+        minW="25rem"
+        flexDirection="column"
+        m="auto"
+      >
+        <Text
+          letterSpacing={2}
+          color="yellow.500"
+          fontWeight="extrabold"
+          colorScheme="yellow"
+        >
+          NIVEL {level}
         </Text>
         <Progress
-          mt={1}
+          my={1}
           borderRadius="20px"
           colorScheme="yellow"
           min={0}
           max={100}
           hasStripe
           isAnimated
-          value={80}
+          value={exp}
         />
+        <Text
+          letterSpacing={2}
+          color="yellow.500"
+          fontWeight="extrabold"
+          colorScheme="yellow"
+        >
+          {isUserAtMaxLevel && "LEVEL MÁXIMO"}
+          {!isUserAtMaxLevel && `EXP ${exp}`}
+        </Text>
       </Flex>
 
       <Navigation />
