14e2ef1f6e4c919f587e48fb9666088001b7daaf
Felipe <felipe1180@hotmail.com>
2022-06-08 16:06:33 -0300

    feat(experience): :sparkles: use experience info hook

---
+++ src/hooks/use-experience-info.tsx
@@ -0,0 +1,25 @@
+import { MAX_LEVEL } from "@constants/exp";
+import { useAppSelector } from "@redux/hooks";
+import { selectExp } from "@redux/slices/exp";
+import { useCallback, useMemo } from "react";
+
+export function useExperienceInfo() {
+  const { level, exp, levelIncreased } = useAppSelector(selectExp);
+
+  const isUserAtMaxLevel = useMemo(() => level === MAX_LEVEL, [level]);
+
+  const isUserAtThisLevel = useCallback(
+    (userLevel: number) => {
+      return level === userLevel;
+    },
+    [level]
+  );
+
+  return {
+    isUserAtMaxLevel,
+    exp,
+    levelIncreased,
+    level,
+    isUserAtThisLevel,
+  };
+}
