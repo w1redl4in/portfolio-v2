6894187f64bb37cf45c2de95747fae4a7f8f4fd0
Felipe <felipe1180@hotmail.com>
2022-06-08 16:05:35 -0300

    chore(layout): :bulb: commented footer from layout

--- src/_layouts/default.tsx
+++ src/_layouts/default.tsx
@@ -1,16 +1,19 @@
 import { Flex } from "@chakra-ui/react";
-import { Footer } from "@components/Footer";
+// import { Footer } from "@components/Footer";
 import { Header } from "@components/Header";
+import { useCountExperience } from "hooks/use-count-experience";
 
 interface DefaultLayoutProps {
   children: React.ReactNode;
 }
 export function DefaultLayout({ children }: DefaultLayoutProps) {
+  useCountExperience();
+
   return (
     <Flex justifyContent="space-between" flexDirection="column" minH="100vh">
       <Header />
       {children}
-      <Footer />
+      {/* <Footer /> */}
     </Flex>
   );
 }
