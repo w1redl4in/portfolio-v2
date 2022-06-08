c4f404d75ad5aae78191e7ee99df4a6835ee7a97
Felipe <felipe1180@hotmail.com>
2022-06-08 00:51:10 -0300

    feat(gamification): :sparkles: introduction gamification modal

---
+++ src/gamification/modals/index.tsx
@@ -0,0 +1,5 @@
+import { GamificationIntroductionModal } from "./introduction";
+
+export function GamificationModalRoot() {
+  return <GamificationIntroductionModal />;
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import { GamificationModalRoot } from "gamification/modals";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
       <Component {...pageProps} />
+      <GamificationModalRoot />
     </ChakraProvider>
   );
 }
