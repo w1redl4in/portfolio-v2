997c70481e00d3f264d26290bca735820e568c14
Felipe <felipe1180@hotmail.com>
2022-06-08 16:06:19 -0300

    feat(experience): :sparkles: use count experience hook

---
+++ src/hooks/use-count-experience.ts
@@ -0,0 +1,33 @@
+import { useAppSelector } from "@redux/hooks";
+import { increaseExp, selectExp } from "@redux/slices/exp";
+import {
+  openGamificationModal,
+  selectGamificationModal,
+} from "@redux/slices/gamification-modal";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { MAX_LEVEL } from "@constants/exp";
+
+export function useCountExperience() {
+  const dispatch = useDispatch();
+
+  const { isGamificationModalOpen } = useAppSelector(selectGamificationModal);
+
+  const { levelIncreased, level } = useAppSelector(selectExp);
+
+  useEffect(() => {
+    if (!isGamificationModalOpen && level < MAX_LEVEL) {
+      const interval = setInterval(() => {
+        dispatch(increaseExp());
+      }, 1000);
+
+      return () => clearInterval(interval);
+    }
+  }, [dispatch, isGamificationModalOpen, level]);
+
+  useEffect(() => {
+    if (levelIncreased) {
+      dispatch(openGamificationModal(level));
+    }
+  }, [dispatch, level, levelIncreased]);
+}
