3c9cd45dd8b8ec79ffa476135af9013f6c7c91bb
Felipe <felipe1180@hotmail.com>
2022-06-13 15:47:22 -0300

    feat(about): :sparkles: about container

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import { DefaultLayout } from "@layouts/default";
-import { Flex, Heading, Text } from "@chakra-ui/react";
+import { Flex, Heading, Image, Text } from "@chakra-ui/react";
 import { CSSProperties } from "react";
 import { useExperienceInfo } from "hooks/use-experience-info";
 
@@ -101,6 +101,7 @@ const Home: NextPage = () => {
   return (
     <DefaultLayout>
       <Flex
+        p={1}
         flexDirection="column"
         textAlign="center"
         justifyContent="center"
@@ -112,17 +113,61 @@ const Home: NextPage = () => {
         {isUserAtThisLevel(2) &&
           array.map((a, key) => <span key={key} style={a}></span>)}
 
-        <Heading>👋 Olá, Eu sou o Felipe Austríaco</Heading>
+        <Heading fontFamily="Heebo">👋 Olá, Eu sou o Felipe Austríaco</Heading>
         <br />
-        <Heading lineHeight={1.2} size="3xl">
+        <Heading fontFamily="Heebo" lineHeight={1.2} size="3xl">
           Software Engineer <br /> lorem ipsum dolor <br /> lorem ipsum dolor
           lorem ipsum dolor
         </Heading>
         <br />
-        <Text fontWeight="medium">
+        <Text fontFamily="Heebo" fontWeight="medium">
           lorem ipsum dolor lorem ipsum dolor @{" "}
           <strong style={{ color: "#ffd900" }}>Mercado Livre</strong>
         </Text>
+      </Flex>
+
+      <Flex
+        m="auto"
+        alignItems="center"
+        justifyContent="center"
+        minHeight="calc(100vh - 108px)"
+        flexDirection="column"
+        maxW="50rem"
+        w="100%"
+      >
+        <Text
+          mx="auto"
+          fontSize="4xl"
+          fontWeight="bold"
+          bgGradient="linear(to-r, #00F5A0, #00D9F5)"
+          bgClip="text"
+          fontFamily="Heebo"
+        >
+          Sobre mim
+        </Text>
+        <Flex
+          justifyContent="center"
+          flexDirection={["column", "column", "row"]}
+          alignItems="center"
+          py={5}
+          m="auto"
+        >
+          <Image
+            borderRadius="50%"
+            opacity={0.5}
+            alignSelf="center"
+            src="https://avatars.githubusercontent.com/u/43390533?v=4"
+            alt="me"
+          />
+          <Text fontFamily="Heebo" p={10}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br />{" "}
+            Gravida morbi pulvinar sit morbi viverra nisl sagittis, placerat.{" "}
+            <br />
+            Non blandit cras fames suspendisse iaculis vitae. Id semper in
+            tempus sed posuere leo. <br /> Dui sed viverra sit in tellus
+            interdum aliquet nunc congue.
+          </Text>
+        </Flex>
       </Flex>
     </DefaultLayout>
   );
