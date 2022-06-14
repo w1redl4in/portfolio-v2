f252d105f355aee7f5e4e9e43bcc56840a1c2ab1
Felipe <felipe1180@hotmail.com>
2022-06-14 17:07:08 -0300

    refactor(home): :recycle: add carreer section and commented about me

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import { DefaultLayout } from "@layouts/default";
-import { Flex, Heading, Image, Text } from "@chakra-ui/react";
+import { Flex, Heading, Image, Stack, Text } from "@chakra-ui/react";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
@@ -30,7 +31,7 @@ const Home: NextPage = () => {
         </Text>
       </Flex>
 
-      <Flex
+      {/* <Flex
         m="auto"
         alignItems="center"
         justifyContent="center"
@@ -72,6 +73,125 @@ const Home: NextPage = () => {
             interdum aliquet nunc congue.
           </Text>
         </Flex>
+      </Flex> */}
+
+      <Flex
+        alignItems="center"
+        justifyContent="center"
+        minHeight="calc(100vh - 108px)"
+      >
+        <Stack
+          paddingX={10}
+          spacing={["10rem", "10rem", "10rem", "1rem"]}
+          maxW="70rem"
+          w="100%"
+        >
+          <Text
+            fontSize="4xl"
+            fontWeight="medium"
+            bgGradient="linear(to-r, #FF8008, #FFC837)"
+            bgClip="text"
+            fontFamily="Heebo"
+          >
+            Carreira
+          </Text>
+          <Flex
+            flexDirection={["column", "column", "column", "row"]}
+            alignItems="center"
+            justifyContent="space-between"
+          >
+            <Stack textAlign={["center", "center", "center", "left"]}>
+              <Text fontFamily="Heebo" color="white" fontWeight="semibold">
+                Analisa de Desenvolvimento de Software
+              </Text>
+              <Flex>
+                <Link href="test" color="white">
+                  <Text
+                    cursor="pointer"
+                    textDecor="underline"
+                    fontWeight="medium"
+                    fontFamily="Heebo"
+                  >
+                    Hub Fintech / Fintech Magalu / Magalu
+                  </Text>
+                </Link>
+                <Text
+                  fontWeight="medium"
+                  fontFamily="Heebo"
+                  color="textSecondary"
+                >
+                  &nbsp;• Barueri, São Paulo
+                </Text>
+              </Flex>
+              <Text
+                fontWeight="medium"
+                fontFamily="Heebo"
+                color="textSecondary"
+              >
+                Jan 2020 – May 2022
+              </Text>
+            </Stack>
+            <Image
+              height="auto"
+              borderRadius="10px"
+              maxW="400px"
+              w="100%"
+              src="hub-fintech.png"
+              alt="hub fintech empresa"
+            />
+          </Flex>
+          <Flex
+            flexDirection={[
+              "column-reverse",
+              "column-reverse",
+              "column-reverse",
+              "row",
+            ]}
+            alignItems="center"
+            justifyContent="space-between"
+          >
+            <Image
+              height="auto"
+              borderRadius="10px"
+              maxW="400px"
+              w="100%"
+              src="meli.png"
+              alt="hub fintech empresa"
+            />
+            <Stack textAlign={["center", "center", "center", "right"]}>
+              <Text fontFamily="Heebo" color="white" fontWeight="semibold">
+                Software Engineer
+              </Text>
+              <Flex>
+                <Link href="test">
+                  <Text
+                    cursor="pointer"
+                    textDecor="underline"
+                    fontWeight="medium"
+                    fontFamily="Heebo"
+                    color="brand"
+                  >
+                    Mercado Livre
+                  </Text>
+                </Link>
+                <Text
+                  fontWeight="medium"
+                  fontFamily="Heebo"
+                  color="textSecondary"
+                >
+                  &nbsp;• Osasco, São Paulo
+                </Text>
+              </Flex>
+              <Text
+                fontWeight="medium"
+                fontFamily="Heebo"
+                color="textSecondary"
+              >
+                Jun 2022 – At moment
+              </Text>
+            </Stack>
+          </Flex>
+        </Stack>
       </Flex>
     </DefaultLayout>
   );
