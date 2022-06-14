3733625073909a28590c5e97c7cac080b1bbfbd9
Felipe <felipe1180@hotmail.com>
2022-06-14 19:01:09 -0300

    chore(introduction): :fire: removing unused code lines

--- src/gamification/modals/introduction/index.tsx
+++ src/gamification/modals/introduction/index.tsx
@@ -1,32 +1,5 @@
-import { Text, Stack, HStack, Flex } from "@chakra-ui/react";
-import { useMemo } from "react";
+import { Text, Stack, Flex } from "@chakra-ui/react";
 export function GamificationIntroductionModal() {
-  const icons = useMemo(() => {
-    return [
-      {
-        text: "",
-        color: "red.400",
-      },
-      {
-        text: "Você ganhará exp durante a navegação e liberará novas áreas e efeitos para conhecer mais sobre mim. ",
-        color: "blue.400",
-      },
-
-      {
-        text: "Logo acima no cabeçalho você verá sua barra de experiência juntamente com o seu nível. ⬆️",
-        color: "gray.400",
-      },
-      {
-        text: "Para cada segundo de navegação você ganhará 10 pontos de exp, boa sorte e ótima navegação! ⌛",
-        color: "orange.400",
-      },
-      {
-        text: "Os pontos só começarão a contabilizar após fechar o modal. ❎",
-        color: "green.400",
-      },
-    ];
-  }, []);
-
   return (
     <Stack textAlign="center">
       <Text color="white" fontFamily="Heebo">
