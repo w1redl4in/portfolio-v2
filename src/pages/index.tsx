ca74f9030878a5cbfd83b42fff955db16d5a7947
Felipe <felipe1180@hotmail.com>
2022-06-15 18:10:25 -0300

    chore(modules): :truck: rename career to camelcase

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import { DefaultLayout } from "@layouts/default";
 import { Introduction } from "@modules/root/Introduction";
 import { AboutMe } from "@modules/root/AboutMe";
-import { Career } from "@modules/root/career/Index";
+import { Career } from "@modules/root/Career/Index";
 
 const Home: NextPage = () => {
   return (
