e63a44bbb90bc1473e163ac1c07208782ed7d4f3
Felipe <felipe1180@hotmail.com>
2022-06-15 18:09:52 -0300

    chore(modules): :truck: rename about-me to camelcase

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import { DefaultLayout } from "@layouts/default";
 import { Introduction } from "@modules/root/Introduction";
-import { AboutMe } from "@modules/root/about-me";
+import { AboutMe } from "@modules/root/AboutMe";
 import { Career } from "@modules/root/career/Index";
 
 const Home: NextPage = () => {
