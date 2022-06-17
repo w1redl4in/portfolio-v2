d4c8e9dc660f1dd6999ccf90d8e7a8781a7661ae
Felipe <felipe1180@hotmail.com>
2022-06-17 02:24:40 -0300

    chore(gamification): :truck: root modal in correct folder

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
-import { GamificationModalRoot } from "@gamification/index";
+import { GamificationModalRoot } from "@gamification/modals";
 import { Provider } from "react-redux";
 import { store } from "@redux/store";
 import { Fonts } from "@components/Fonts";
