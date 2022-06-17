2a7848c99b9abcf8878cddf7a52b7415f9e5b5d7
Felipe <felipe1180@hotmail.com>
2022-06-17 01:49:10 -0300

    feat(level-2-reward): :sparkles: text gradient and cat level up lottie

--- src/gamification/modals/level-2/index.tsx
+++ src/gamification/modals/level-2/index.tsx
@@ -1,19 +1,45 @@
 import { Heading, Stack, Text } from "@chakra-ui/react";
+import { Lottie } from "@components/Lottie";
+import LevelUp from "@lotties/level-up.json";
+import FancyText from "@carefully-coded/react-text-gradient";
+import { useResponsive } from "@hooks/use-responsive";
+import { useMemo } from "react";
 
 export function GamificationModalLevel2() {
+  const { isMobile } = useResponsive();
+
+  const lottieWidthAndHeight = useMemo(() => {
+    if (isMobile) return 150;
+    return 200;
+  }, [isMobile]);
+
   return (
     <Stack textAlign="center">
-      <Heading color="yellow.400" size="lg">
-        Level up!
+      <Lottie
+        animationData={LevelUp}
+        width={lottieWidthAndHeight}
+        height={lottieWidthAndHeight}
+        speed={10}
+      />
+      <Heading fontFamily="Bungee" color="yellow.400" size="lg">
+        <FancyText
+          gradient={{ from: "#FFE259", to: "#FFA751", type: "linear" }}
+          animate
+          animateDuration={500}
+        >
+          Level Up!
+        </FancyText>
       </Heading>
-      <Text color="white">Você alcançou o nível 2!</Text>
-
-      <Text color="white">
+      <Text fontFamily="Bungee" color="white">
+        Você alcançou o nível 2!
+      </Text>
+      <Text fontFamily="Heebo" color="white">
         Agora conseguirá ver as tecnologias que mais gosto <br />
         flutuando pelo site.
       </Text>
-
-      <Text color="white">Divirta-se!</Text>
+      <Text fontFamily="Heebo" color="white">
+        Divirta-se!
+      </Text>
     </Stack>
   );
 }
