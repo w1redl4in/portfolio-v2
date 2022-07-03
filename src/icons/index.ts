dcdbee9157778c331140764579c9d69f3f98052e
Felipe <felipe1180@hotmail.com>
2022-07-03 18:36:36 -0300

    feat(icons): :sparkles: new icons family

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -2,3 +2,5 @@ export * from "react-icons/fa";
 export * from "react-icons/ai";
 export * from "react-icons/fi";
 export * from "react-icons/md";
+export * from "react-icons/gr";
+export * from "react-icons/gi";
