09fe51248a31d1d64ca3f30ea97040aaef97c2bb
w1redl4in <felipe.austriaco@mercadolivre.com>
2022-07-06 18:46:40 -0300

    feat(pages): :sparkles: more metatags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,23 @@ import { DefaultLayout } from "@layouts/default";
 import { Introduction } from "@modules/root/Introduction";
 import { AboutMe } from "@modules/root/AboutMe";
 import { Career } from "@modules/root/Career/Index";
+import Head from "next/head";
 
 const Home: NextPage = () => {
   return (
     <DefaultLayout>
+      <Head>
+        <title>Felipe Austríaco - Página Inicial</title>
+        <meta name="author" content="Felipe Austríaco" />
+        <meta
+          name="description"
+          content="My portfolio with personal info, projects and articles"
+        />
+        <meta
+          name="keywords"
+          content="Felipe Austríaco, Austríaco, React, ReactJS, React.JS, Javascript, JS, js, Node, Node.js, Node.JS, HTML, HTML5, CSS, CSS3, Next, Nextjs, Next.js, CMS, Prismic CMS, Typescript, TypeScript, ts, portfolio, Portfolio, Hooks, React-hooks"
+        />
+      </Head>
       <Introduction />
       <AboutMe />
       <Career />
