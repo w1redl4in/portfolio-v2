6eab4bd49bc506044dfc15330c88e44b11ca5cb9
w1redl4in <felipe.austriaco@mercadolivre.com>
2022-07-06 18:51:17 -0300

    feat(_document): :sparkles: more metadata :D :D :D

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -21,6 +21,18 @@ class MyDocument extends Document {
       <Html lang="pt-BR">
         <Head>
           <link rel="shortcut icon" href="/images/favicon.ico" />
+          <meta property="og:title" content="Felipe Austríaco" />
+          <meta property="og:image" content="/images/me.jpeg" />
+          <meta
+            property="og:description"
+            content="Meu portfolio com alguma informações pessoais, projetos/estudos e artigos!"
+          />
+          <meta
+            property="og:url"
+            content="Programação frontend, carreira, dicas"
+          />
+          <meta property="og:image:width" content="1200" />
+          <meta property="og:image:height" content="627" />
         </Head>
         <body>
           <Main />
