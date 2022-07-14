b589b4d9229c79d49dd84eae1c223e0e4ba1afea
w1redl4in <felipe.austriaco@mercadolivre.com>
2022-07-14 18:34:34 -0300

    feat(_app): :sparkles: add prismic nextjs preview

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,21 +5,36 @@ import { store } from "@redux/store";
 import { Fonts } from "@components/Fonts";
 import { theme } from "@styles/theme";
 import { GamificationBrain } from "brain";
+import { PrismicPreview } from "@prismicio/next";
+import { PrismicProvider } from "@prismicio/react";
+import { linkResolver, repositoryName } from "../../prismicio";
 import TopBarProgress from "nextjs-progressbar";
+import Link from "next/link";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Provider store={store}>
-        <Fonts />
-        <TopBarProgress
-          color="linear-gradient(to right, #FF8008, #FFC837)"
-          height={3}
-        />
-        <Component {...pageProps} />
-        <GamificationBrain />
-      </Provider>
-    </ChakraProvider>
+    <PrismicProvider
+      linkResolver={linkResolver}
+      internalLinkComponent={({ href, children, ...props }) => (
+        <Link href={href}>
+          <a {...props}>{children}</a>
+        </Link>
+      )}
+    >
+      <PrismicPreview repositoryName={repositoryName}>
+        <ChakraProvider theme={theme}>
+          <Provider store={store}>
+            <Fonts />
+            <TopBarProgress
+              color="linear-gradient(to right, #FF8008, #FFC837)"
+              height={3}
+            />
+            <Component {...pageProps} />
+            <GamificationBrain />
+          </Provider>
+        </ChakraProvider>
+      </PrismicPreview>
+    </PrismicProvider>
   );
 }
 
