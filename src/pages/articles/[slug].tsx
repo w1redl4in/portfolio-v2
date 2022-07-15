5362712dfeca427b5162db0f31cff43851f980fa
w1redl4in <felipe.austriaco@mercadolivre.com>
2022-07-15 14:22:51 -0300

    feat(article): :sparkles: add back button

--- src/pages/articles/[slug].tsx
+++ src/pages/articles/[slug].tsx
@@ -1,9 +1,20 @@
-import { Box, Heading, Stack, Image, Flex } from "@chakra-ui/react";
+import {
+  Box,
+  Heading,
+  Stack,
+  Image,
+  Flex,
+  Link,
+  Text,
+  Icon,
+} from "@chakra-ui/react";
 import { DynamicSliceMachine } from "@components/SliceMachine";
 import { DefaultLayout } from "@layouts/default";
 import { RichText } from "prismic-reactjs";
 import { getPrismicClient } from "services/prismic";
 import { SEO } from "@components/SEO";
+import NextLink from "next/link";
+import { AiOutlineRollback } from "@icons";
 
 type ArticleProps = {
   article: {
@@ -32,7 +43,24 @@ const Article: React.FC<ArticleProps> = ({ article }) => {
             alt={article.altThumbnail}
           />
         </Flex>
+
         <Box p="2rem" maxW="50rem" w="100%" margin="0 auto">
+          <Box mb="2rem">
+            <NextLink href="/articles" passHref>
+              <Link display="flex" color="white" alignItems="center">
+                <Icon as={AiOutlineRollback} mx="2px" w={9} h={9} />
+                <Text
+                  transition="all 200ms"
+                  fontSize="2xl"
+                  fontFamily="Heebo"
+                  fontWeight="bold"
+                >
+                  Voltar
+                </Text>
+              </Link>
+            </NextLink>
+          </Box>
+
           <Heading color="brand">{article.title}</Heading>
           <Stack spacing="3rem">
             <DynamicSliceMachine slices={article.slices} />
