31a9a2df0487a558ea8c76abe33c4056c1fc7789
w1redl4in <felipe.austriaco@mercadolivre.com>
2022-07-15 13:04:09 -0300

    chore(slicemachine): :wrench: auto generated files

---
+++ .slicemachine/assets/slices/Code/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/Code';
+
+export default {
+  title: 'slices/Code'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"code","items":[],"primary":{"Code":[{"type":"paragraph","text":"Laboris aute pariatur veniam ullamco ipsum anim sit nostrud enim amet velit eu. Dolore duis officia voluptate laboris enim ad nulla aliqua laborum occaecat occaecat ex et. Qui voluptate ex tempor.","spans":[]}],"Colors":"red"},"id":"_Default"}} />
+_Default.storyName = 'Default'
--- .slicemachine/assets/slices/TextBlock/index.stories.js
+++ .slicemachine/assets/slices/TextBlock/index.stories.js
@@ -5,5 +5,5 @@ export default {
 }
 
 
-export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"text_block","items":[],"primary":{"title":[{"type":"heading1","text":"Transition e-business action-items","spans":[]}],"description":[{"type":"paragraph","text":"Do enim elit sint fugiat sint. Culpa proident nostrud eu.","spans":[]}]},"id":"_Default"}} />
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"text_block","items":[],"primary":{"title":[{"type":"heading2","text":"Enhance B2B models","spans":[]}],"description":[{"type":"paragraph","text":"Est nostrud ad consequat pariatur. Ipsum non qui tempor veniam non laborum aliqua anim do elit officia enim fugiat. Ea fugiat eiusmod sint nisi proident et eiusmod pariatur.","spans":[]}],"Code":[{"type":"paragraph","text":"Exercitation culpa proident ad sunt est consectetur anim esse. Labore Lorem consectetur cupidatat aute officia fugiat commodo sunt consectetur. Nisi consectetur veniam ipsum adipisicing dolor pariatur consectetur cupidatat elit esse ex dolore sint.","spans":[]}],"codeLanguage":[{"type":"paragraph","text":"Occaecat anim ad occaecat mollit exercitation enim proident fugiat exercitation labore.","spans":[]}]},"id":"_Default"}} />
 _Default.storyName = 'Default'
