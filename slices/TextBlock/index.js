b96af999b3dbe033b83589e95bfc377d9793878e
w1redl4in <felipe.austriaco@mercadolivre.com>
2022-07-15 13:04:39 -0300

    refactor(slice): :recycle: textblock with code slice

--- slices/TextBlock/index.js
+++ slices/TextBlock/index.js
@@ -1,15 +1,29 @@
 import React from "react";
 import { RichText } from "prismic-reactjs";
 import { Stack, Heading, Text } from "@chakra-ui/react";
+import Syntax from "react-syntax-highlighter";
+import { gruvboxDark } from "react-syntax-highlighter/dist/cjs/styles/hljs";
 
 const TextBlock = ({ slice }) => (
   <Stack as="section">
-    <Heading color="brand">
+    {console.log("slices", slice)}
+    <Heading size="lg" color="brand">
       <RichText render={slice.primary.title} />
     </Heading>
     <Text color="textSecondary">
       <RichText render={slice.primary.description} />
     </Text>
+    {slice.primary.Code.length > 0 && (
+      <Syntax
+        customStyle={{
+          padding: "2rem 1rem",
+        }}
+        language="typescript"
+        style={gruvboxDark}
+      >
+        {RichText.asText(slice.primary.Code)}
+      </Syntax>
+    )}
   </Stack>
 );
 
