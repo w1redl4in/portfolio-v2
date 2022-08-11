64f7a1c0b38a54fc48451a948838cebe18fd877e
w1redl4in <felipe.austriaco@mercadolivre.com>
2022-08-11 14:39:17 -0300

    refactor(index): :recycle: using SEO component instead of hand-writed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,28 +3,19 @@ import { DefaultLayout } from "@layouts/default";
 import { Introduction } from "@modules/root/Introduction";
 import { AboutMe } from "@modules/root/AboutMe";
 import { Career } from "@modules/root/Career/Index";
-import { SocialMedias } from "@modules/root/SocialMedias";
-import Head from "next/head";
+import { SEO } from "@components/SEO";
 
 const Home: NextPage = () => {
   return (
     <DefaultLayout>
-      <Head>
-        <title>Felipe Austríaco - Página Inicial</title>
-        <meta name="author" content="Felipe Austríaco" />
-        <meta
-          name="description"
-          content="My portfolio with personal info, projects and articles"
-        />
-        <meta
-          name="keywords"
-          content="Felipe Austríaco, Austríaco, React, ReactJS, React.JS, Javascript, JS, js, Node, Node.js, Node.JS, HTML, HTML5, CSS, CSS3, Next, Nextjs, Next.js, CMS, Prismic CMS, Typescript, TypeScript, ts, portfolio, Portfolio, Hooks, React-hooks"
-        />
-      </Head>
+      <SEO
+        title="Felipe Austríaco - Página Inicial"
+        image="/images/me.jpg"
+        description="Meu portfólio com informações pessoais, projetos e artigos"
+      />
       <Introduction />
       <AboutMe />
       <Career />
-      <SocialMedias />
     </DefaultLayout>
   );
 };
