2185bc7c3e73b2e154fcfbe779a3223b89334bc3
Felipe <felipe.austriaco@mercadolivre.com>
2024-01-05 15:52:06 -0300

    chore: :wrench: configuring unsplash api

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,5 @@
 export const config = {
   endpoint: String(process.env.PRISMIC_API_ENDPOINT),
   accessToken: String(process.env.PRISMIC_ACCESS_TOKEN),
+  unsplashAccessKey: String(process.env.UNSPLASH_ACCESS_KEY),
 };
---
+++ src/services/unsplash.ts
@@ -0,0 +1,25 @@
+import { createApi } from "unsplash-js";
+import { config } from "config";
+import * as nodeFetch from "node-fetch";
+
+export const unsplashApi = createApi({
+  accessKey: config.unsplashAccessKey,
+  fetch: nodeFetch.default as unknown as typeof fetch,
+});
+
+export async function getUnsplashImage(
+  keywords: string,
+  orientation: "landscape" = "landscape"
+) {
+  const response = await unsplashApi.search.getPhotos({
+    query: keywords,
+    orientation,
+  });
+
+  const image = response.response?.results[3];
+
+  return {
+    thumbnail: image?.urls?.full,
+    altThumbnail: image?.alt_description,
+  };
+}
