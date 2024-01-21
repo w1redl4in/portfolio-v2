f8ef5742f29aa24da7d31399b58ab61fde00551f
Felipe <felipe.austriaco@mercadolivre.com>
2024-01-21 11:55:20 -0300

    feat(slices): :sparkles: add new heading sizes and different style for links descriptions

--- slices/TextBlock/index.js
+++ slices/TextBlock/index.js
@@ -8,31 +8,51 @@ const headingTranslations = {
   heading1: "xl",
   heading2: "lg",
   heading3: "md",
+  heading4: "sm",
+  heading5: "xs",
 };
 
-const TextBlock = ({ slice }) => (
-  <Stack as="section">
-    <Heading
-      size={headingTranslations[slice.primary.title[0]?.type]}
-      color="brand"
-    >
-      <RichText render={slice.primary.title} />
-    </Heading>
-    <Text color="textSecondary">
-      <RichText render={slice.primary.description} />
-    </Text>
-    {slice.primary.Code.length > 0 && (
-      <Syntax
-        customStyle={{
-          padding: "2rem 1rem",
-        }}
-        language={slice.primary.codeLanguage[0]?.text}
-        style={darcula}
+function currentDescriptionHasLinks(description) {
+  return description.spans.some((span) => span.type === "hyperlink");
+}
+
+const TextBlock = ({ slice }) => {
+  return (
+    <Stack as="section" spacing="0.5">
+      <Heading
+        size={headingTranslations[slice.primary.title[0]?.type]}
+        color="brand"
       >
-        {RichText.asText(slice.primary.Code)}
-      </Syntax>
-    )}
-  </Stack>
-);
+        <RichText render={slice.primary.title} />
+      </Heading>
+
+      {slice.primary.description.map((item) => {
+        return (
+          <Text
+            textDecor={currentDescriptionHasLinks(item) && "underline"}
+            color={
+              currentDescriptionHasLinks(item) ? "#E2E8F0" : "textSecondary"
+            }
+            key={item.text}
+          >
+            <RichText render={[item]} />
+          </Text>
+        );
+      })}
+
+      {slice.primary.Code.length > 0 && (
+        <Syntax
+          customStyle={{
+            padding: "2rem 1rem",
+          }}
+          language={slice.primary.codeLanguage[0]?.text}
+          style={darcula}
+        >
+          {RichText.asText(slice.primary.Code)}
+        </Syntax>
+      )}
+    </Stack>
+  );
+};
 
 export default TextBlock;
